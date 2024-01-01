38172d5987fe7f0a1946e2e72af55a413f5b7b04
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2024-01-01 14:23:19 -0300

    refactor: input compose component

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -9,23 +9,25 @@ import {
   SquareStack,
   Users,
 } from 'lucide-react'
-import { Logo } from './logo'
-import { NavItem } from './nav-item'
+
 import { UsedSpaceWidget } from './used-space-widget'
+import { NavItem } from './nav-item'
 import { Profile } from './profile'
+import * as Input from '../input'
+import { Logo } from './logo'
 
 export function Sidebar() {
   return (
     <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
-      <div className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-2 py-2 shadow-sm">
-        <Search className="h-5 w-5 text-zinc-500" />
-        <input
-          className="flex-1 border-0  bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
-          placeholder="Search"
-          type="text"
-        />
-      </div>
+
+      <Input.Root>
+        <Input.Prefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </Input.Prefix>
+        <Input.Control placeholder="Search" />
+      </Input.Root>
+
       <nav className="space-y-0.5">
         <NavItem title="Home" icon={Home} />
         <NavItem title="Dashboard" icon={BarChart} />

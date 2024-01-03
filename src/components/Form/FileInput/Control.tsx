1673f74f7687d8a18455cadd58e96f4b0a9ee857
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2024-01-03 03:57:45 -0300

    feat: image preview from file input

--- src/components/Form/FileInput/Control.tsx
+++ src/components/Form/FileInput/Control.tsx
@@ -1,7 +1,28 @@
-import { ComponentProps } from 'react'
+'use client'
+
+import { ChangeEvent, ComponentProps } from 'react'
+import { useFileInput } from './Root'
 
 type ControlProps = ComponentProps<'input'>
 
 export function Control(props: ControlProps) {
-  return <input type="file" className="sr-only" id="photo" {...props} />
+  const { id, onFilesSelected } = useFileInput()
+
+  const handleFilesSelected = (event: ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files?.length) return
+
+    const files = Array.from(event.target.files)
+
+    onFilesSelected(files)
+  }
+
+  return (
+    <input
+      type="file"
+      className="sr-only"
+      onChange={handleFilesSelected}
+      id={id}
+      {...props}
+    />
+  )
 }
--- src/components/Form/FileInput/ImagePreview.tsx
+++ src/components/Form/FileInput/ImagePreview.tsx
@@ -1,9 +1,34 @@
+'use client'
+
 import { User } from 'lucide-react'
+import { useFileInput } from './Root'
+import { useMemo } from 'react'
+import Image from 'next/image'
 
 export function ImagePreview() {
+  const { files } = useFileInput()
+
+  const previewURL = useMemo(() => {
+    if (files.length === 0) return null
+
+    return URL.createObjectURL(files[0])
+  }, [files])
+
+  if (previewURL === null) {
+    return (
+      <div className="flex h-16 w-16 items-center justify-center rounded-full bg-violet-50">
+        <User className="h-8 w-8 text-violet-500" />
+      </div>
+    )
+  }
+
   return (
-    <div className="flex h-16 w-16 items-center justify-center rounded-full bg-violet-50">
-      <User className="h-8 w-8 text-violet-500" />
-    </div>
+    <Image
+      src={previewURL}
+      alt=""
+      width={64}
+      height={64}
+      className="h-16 w-16 rounded-full object-cover"
+    />
   )
 }

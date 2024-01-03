5756b937464c30e3ce574b1324fa5b0939437cb3
Rodolfo Menezes <rodolfosouzaofc@gmail.com>
2024-01-03 14:45:05 -0300

    style: select down animation

--- src/components/Form/Select/index.tsx
+++ src/components/Form/Select/index.tsx
@@ -22,7 +22,7 @@ export function Select({ children, placeholder, ...rest }: SelectProps) {
           side="bottom"
           position="popper"
           sideOffset={8}
-          className="z-10 w-[--radix-select-trigger-width] overflow-hidden rounded-lg border border-zinc-200 bg-white shadow-sm"
+          className="animate-slideDownAndFade z-10 w-[--radix-select-trigger-width] overflow-hidden rounded-lg border border-zinc-200 bg-white shadow-sm"
         >
           <SelectRadix.Viewport>{children}</SelectRadix.Viewport>
         </SelectRadix.Content>

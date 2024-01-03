129764de4ccb4512f06a1c331fcd4d54c968ec3a
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2024-01-03 05:49:06 -0300

    feat: textarea component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,10 @@ import { SettingsTabs } from '@/components/SettingsTabs'
 
 import * as Input from '@/components/input'
 import * as FileInput from '@/components/Form/FileInput'
-import { Mail } from 'lucide-react'
+import { Bold, Italic, Link, List, ListOrdered, Mail } from 'lucide-react'
 import { Select } from '@/components/Form/Select'
 import { SelectItem } from '@/components/Form/Select/SelectItem'
+import { TextArea } from '@/components/Form/TextArea'
 
 export default function Home() {
   return (
@@ -151,7 +152,59 @@ export default function Home() {
                 Write a short introduction.
               </span>
             </label>
-            <div></div>
+            <div className="space-y-3">
+              <div className="grid grid-cols-2 gap-3">
+                <Select placeholder="" defaultValue="normal">
+                  <SelectItem
+                    text="Normal Text"
+                    value="normal"
+                    defaultChecked
+                  />
+                  <SelectItem text="Markdown" value="md" />
+                </Select>
+
+                <div className="flex items-center gap-1">
+                  <button
+                    type="button"
+                    className="rounded-md p-2 hover:bg-zinc-50"
+                  >
+                    <Bold className="h-4 w-4 text-zinc-500" strokeWidth={3} />
+                  </button>
+                  <button
+                    type="button"
+                    className="rounded-md p-2 hover:bg-zinc-50"
+                  >
+                    <Italic className="h-4 w-4 text-zinc-500" strokeWidth={3} />
+                  </button>
+                  <button
+                    type="button"
+                    className="rounded-md p-2 hover:bg-zinc-50"
+                  >
+                    <Link className="h-4 w-4 text-zinc-500" strokeWidth={3} />
+                  </button>
+                  <button
+                    type="button"
+                    className="rounded-md p-2 hover:bg-zinc-50"
+                  >
+                    <List className="h-4 w-4 text-zinc-500" strokeWidth={3} />
+                  </button>
+                  <button
+                    type="button"
+                    className="rounded-md p-2 hover:bg-zinc-50"
+                  >
+                    <ListOrdered
+                      className="h-4 w-4 text-zinc-500"
+                      strokeWidth={3}
+                    />
+                  </button>
+                </div>
+              </div>
+
+              <TextArea
+                id="bio"
+                defaultValue="I'm a Product Designer based in Melbourne, Australia. I specialise in UX/UI design, brand strategy, and Webflow development."
+              />
+            </div>
           </div>
 
           <div className="grid grid-cols-form gap-6 pt-5">
--- src/components/Form/Select/index.tsx
+++ src/components/Form/Select/index.tsx
@@ -2,16 +2,14 @@
 
 import * as SelectRadix from '@radix-ui/react-select'
 import { ChevronDown } from 'lucide-react'
-import { ReactNode } from 'react'
 
-interface SelectProps {
-  children: ReactNode
+interface SelectProps extends SelectRadix.SelectProps {
   placeholder: string
 }
 
-export function Select({ children, placeholder }: SelectProps) {
+export function Select({ children, placeholder, ...rest }: SelectProps) {
   return (
-    <SelectRadix.Root>
+    <SelectRadix.Root {...rest}>
       <SelectRadix.Trigger className="flex h-11 w-full items-center justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm data-[placeholder]:text-zinc-600">
         <SelectRadix.Value placeholder={placeholder} className="text-black" />
         <SelectRadix.Icon>
@@ -24,7 +22,7 @@ export function Select({ children, placeholder }: SelectProps) {
           side="bottom"
           position="popper"
           sideOffset={8}
-          className="z-10 w-[--radix-select-trigger-width] overflow-hidden rounded-lg border border-zinc-200 bg-white"
+          className="z-10 w-[--radix-select-trigger-width] overflow-hidden rounded-lg border border-zinc-200 bg-white shadow-sm"
         >
           <SelectRadix.Viewport>{children}</SelectRadix.Viewport>
         </SelectRadix.Content>
